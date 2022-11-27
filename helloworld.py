25d8f4a0884b65f3a9e3a1c366c410f9219fe73e
vipul <vipulkhapekar16@gmail.com>
2022-11-27 14:14:32 +0530

    configured django env

---
+++ helloworld.py
@@ -0,0 +1,1 @@
+print("Welcome to Atom Editor")
---
+++ profilesapi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProfilesapiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'profilesapi'
